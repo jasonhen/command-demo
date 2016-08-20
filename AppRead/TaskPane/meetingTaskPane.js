ffdf189051e2fd1d6fbf92c7997c76301b6f3712
jasonhen <jasonhen@microsoft.com>
2016-08-19 21:26:25 -0700

    update

--- AppRead/TaskPane/meetingTaskPane.js
+++ AppRead/TaskPane/meetingTaskPane.js
@@ -1,5 +1,103 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
 
+/// <reference path="../App.js" />
 
 
-$(".ms-Dropdown").Dropdown();
+
+(function () {
+    "use strict";
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            $('button').click(setLabel);
+        });
+    };
+
+    function setLabel() {
+        //  $('#statusLabel').html("Cheer sent!");
+        loadMessageProps(Office.context.mailbox.item);
+    }
+
+    function getCallbackToken() {
+        Office.context.mailbox.getCallbackTokenAsync(cb);
+    }
+
+    function cb(asyncResult) {
+        var token = asyncResult.value;
+        $('#statusLabel').html("<span class='ms-font-l'>"+ token + "</span>");
+    }
+
+    // Take an array of AttachmentDetails objects and
+    // build a list of attachment names, separated by a line-break
+    function buildAttachmentsString(attachments) {
+        if (attachments && attachments.length > 0) {
+            var returnString = "";
+
+            for (var i = 0; i < attachments.length; i++) {
+                if (i > 0) {
+                    returnString = returnString + "<br/>";
+                }
+                returnString = returnString + attachments[i].name;
+            }
+
+            return returnString;
+        }
+
+        return "None";
+    }
+
+    // Format an EmailAddressDetails object as
+    // GivenName Surname <emailaddress>
+    function buildEmailAddressString(address) {
+        return address.displayName;
+    }
+
+    // Take an array of EmailAddressDetails objects and
+    // build a list of formatted strings, separated by a line-break
+    function buildEmailAddressesString(addresses) {
+        if (addresses && addresses.length > 0) {
+            var returnString = "";
+
+            for (var i = 0; i < addresses.length; i++) {
+                if (i > 0) {
+                    returnString = returnString + "<br/>";
+                }
+                returnString = returnString + buildEmailAddressString(addresses[i]);
+            }
+
+            return returnString;
+        }
+
+        return "None";
+    }
+
+    // Load properties from a Message object
+    function loadMessageProps(item) {
+        $('#statusLabel').html("<span class='ms-font-l'>You just sent a cheer to " + buildEmailAddressString(item.from) + "</span>");
+        getCallbackToken()
+    }
+
+})();
+
+// MIT License: 
+
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// ""Software""), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject to 
+// the following conditions: 
+
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software. 
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
